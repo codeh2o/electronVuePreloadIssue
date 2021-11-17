5c4fb9395afeb09157275266230f565eed9390ea
h2o <h2o@example.com>
2021-11-17 14:08:48 +0800

    fix an error

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  transpileDependencies: ['vuetify'],
+  // transpileDependencies: ['vuetify'],
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
       builderOptions: {
         // Add also your database location
         extraResources: ['src', 'src/res/'],
-        preload: 'src/preload.js',
+        preload: { preload: 'src/preload.js' }
       },
       // This line: add knex and sqlite3
       externals: ['knex', 'sqlite3'],
